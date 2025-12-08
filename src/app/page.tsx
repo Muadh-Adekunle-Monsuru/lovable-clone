65150d886a275a330b82463c9be927ed5b5d2be0
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2025-12-08 16:42:31 +0100

    04: Background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import { useTRPC } from '@/trpc/client';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { caller } from '@/trpc/server';
+'use client';
 
-export default async function Page() {
-	const data = await caller.hello({ text: 'hello' });
-	return <div className='font-bold'></div>;
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
+export default function Page() {
+	const trpc = useTRPC();
+	const invoke = useMutation(
+		trpc.invoke.mutationOptions({
+			onSuccess: () => {
+				toast.success('Background Job Started');
+			},
+		})
+	);
+	return (
+		<div className='p-4 max-w-7xl mx-auto'>
+			<Button
+				disabled={invoke.isPending}
+				onClick={() =>
+					invoke.mutate({
+						text: 'John Cena',
+					})
+				}
+			>
+				Invoke BG Job
+			</Button>
+		</div>
+	);
 }
