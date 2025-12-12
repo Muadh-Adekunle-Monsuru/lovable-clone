4fb0328361cee83425df5180fd2f2476e33983db
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2025-12-12 20:28:46 +0100

    05: AI Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useTRPC } from '@/trpc/client';
 import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
 export default function Page() {
+	const [value, setValue] = useState('');
 	const trpc = useTRPC();
 	const invoke = useMutation(
 		trpc.invoke.mutationOptions({
@@ -16,11 +19,12 @@ export default function Page() {
 	);
 	return (
 		<div className='p-4 max-w-7xl mx-auto'>
+			<Input value={value} onChange={(e) => setValue(e.target.value)} />
 			<Button
 				disabled={invoke.isPending}
 				onClick={() =>
 					invoke.mutate({
-						text: 'John Cena',
+						 value: value,
 					})
 				}
 			>
